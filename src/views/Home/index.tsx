55c6d4e44bc30f37164dafd78cd0fddafcf928f0
佩奇 <cqwangrui925@gmail.com>
2025-11-12 16:49:26 +0800

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,21 +1,23 @@
 import Footer from '@/components/Footer'
 import Cover from './components/Cover'
 import Header from './components/Header'
-import Importance from './components/Importance'
-import Supports from './components/Supports'
-import Usage from './components/Usage'
-import WallOfLove from './components/WallOfLove'
+// 注释不需要的组件导入
+// import Importance from './components/Importance'
+// import Supports from './components/Supports'
+// import Usage from './components/Usage'
+// import WallOfLove from './components/WallOfLove'
 
 function Home() {
   return (
     <>
       <Header />
       <Cover />
-      <Importance />
-      <Usage />
-      <Supports />
-      <WallOfLove />
-      <Footer />
+      {/* 注释掉不需要渲染的组件 */}
+      {/* <Importance /> */}
+      {/* <Usage /> */}
+      {/* <Supports /> */}
+      {/* <WallOfLove /> */}
+      {/* <Footer /> */}
     </>
   )
 }
