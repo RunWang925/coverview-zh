eac9c9a831a9d288b01d8c94e61d6b0c0a173f9d
佩奇 <cqwangrui925@gmail.com>
2025-11-12 16:52:09 +0800

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,23 +1,11 @@
-import Footer from '@/components/Footer'
 import Cover from './components/Cover'
 import Header from './components/Header'
-// 注释不需要的组件导入
-// import Importance from './components/Importance'
-// import Supports from './components/Supports'
-// import Usage from './components/Usage'
-// import WallOfLove from './components/WallOfLove'
 
 function Home() {
   return (
     <>
       <Header />
       <Cover />
-      {/* 注释掉不需要渲染的组件 */}
-      {/* <Importance /> */}
-      {/* <Usage /> */}
-      {/* <Supports /> */}
-      {/* <WallOfLove /> */}
-      {/* <Footer /> */}
     </>
   )
 }
