4b20bdbca95e7033d2503e52664225834fe932c2
佩奇 <cqwangrui925@gmail.com>
2025-11-13 10:19:19 +0800

    Update en.ts

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -5,7 +5,6 @@ export default {
   },
   home: {
     description: 'Generate cover images for your blog easily and quickly',
-    descriptionSub: 'In just a few simple steps, you can generate a cover image suitable for your blog, supporting multiple platforms. Give it a try now!',
     goEditor: 'Create Now',
     whyCoverImages: 'Why cover images are more important than you think?',
     reason1: 'Around <b>7 million</b> blog posts are published daily. And with the rise of new-age blogging tools, it will only go up.',
