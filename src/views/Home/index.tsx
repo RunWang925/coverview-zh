948ca80066a7c90ee170fa2eb37e7f52325117c0
佩奇 <cqwangrui925@gmail.com>
2025-11-13 09:58:10 +0800

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -10,10 +10,10 @@ function Home() {
       <footer className="mt-auto bg-indigo-50/80 py-4 border-t border-indigo-200">
         <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-600 text-sm mb-2 md:mb-0">
-            © {new Date().getFullYear()} 野猪佩奇弟弟 | 版权所有
+            © {new Date().getFullYear()} 野猪佩奇弟弟
           </p>
           <a
-            href="https://hexo.814925.xyz"
+            href="https://hexo.814925.xyz"#这里修改跳转关于本站连接 我这里跳转文章说明
             target="_blank"
             rel="noreferrer"
             className="text-indigo-600 hover:text-indigo-800 text-sm font-medium"
